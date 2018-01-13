fdbff95370c50c35d4f74436759a61f7251956a7
Tom Hartman <thomas.lees.hartman@gmail.com>
2018-01-13 13:42:15 -0500

    Fixed the Xephyr scripts
    Fixed multiple typos in the test-awesome-config.sh script. Now
    actually works.

--- test-awesome-config.sh
+++ test-awesome-config.sh
@@ -1,5 +1,5 @@
-!/bin/bash
+#!/bin/bash
 
-Xephyr -ac -br -noreset -scren 1040x768 :1 &
+Xephyr -ac -br -noreset -screen 1040x768 :1 &
 sleep 1
-DISPLAY=:1.0 awesome -c > ~/.cache/awesome/test-stdout 2> ~/.cache/awesome/test-stderr
+DISPLAY=:1.0 awesome -c rc.lua > ~/.cache/awesome/test-stdout 2> ~/.cache/awesome/test-stderr
