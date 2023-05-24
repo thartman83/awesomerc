24f0a74bd97309dd31303db7be3b8bc22826cba7
Tom Hartman <thomas.lees.hartman@gmail.com>
2023-05-23 22:12:10 -0400

    Mostly working
    Fixed screen issuse as well as setup most widgets.

--- rc.lua
+++ rc.lua
@@ -1,22 +1,15 @@
-local gears     = require("gears"                    )
-local gtable    = require("gears.table"              )
-local awful     = require("awful"                    )
-                  require("awful.autofocus"          )
-                  require("awful.hotkeys_popup.keys" )
-local wibox     = require("wibox"                    )
-local beautiful = require("beautiful"                )
-local naughty   = require("naughty"                  )
-local ruled     = require("ruled"                    )
-local menubar   = require("menubar"                  )
-local io        = require("io"                       )
-local dpi       = require("beautiful.xresources"     ).apply_dpi
-
-local weather    = require("awesome-wm-widgets.weather-widget.weather")
-local cpu_widget = require("awesome-wm-widgets.cpu-widget.cpu-widget" )
-local volumebar_widget = require("awesome-wm-widgets.volumebar-widget.volumebar")
-local spotify_widget = require("awesome-wm-widgets.spotify-widget.spotify")
-local docker_widget = require("awesome-wm-widgets.docker-widget.docker")
-local bt_widget = require("awesome-wm-widgets.bluetooth-widget.bluetooth")
+local gears         = require("gears"                    )
+local gtable        = require("gears.table"              )
+local awful         = require("awful"                    )
+                      require("awful.autofocus"          )
+local hotkeys_popup = require("awful.hotkeys_popup.keys" )
+local wibox         = require("wibox"                    )
+local beautiful     = require("beautiful"                )
+local naughty       = require("naughty"                  )
+local ruled         = require("ruled"                    )
+local menubar       = require("menubar"                  )
+local io            = require("io"                       )
+local dpi           = require("beautiful.xresources"     ).apply_dpi
 
 naughty.connect_signal("request::display_error", function(message, startup)
     naughty.notification {
@@ -28,6 +21,25 @@ end)
 
 local conf_dir = gears.filesystem.get_configuration_dir()
 
+local themeName = "multicolor"
+beautiful.init(conf_dir .. "/themes/" .. themeName .. "/theme.lua")
+
+bling = require('bling')
+
+local weather           = require("awesome-wm-widgets.weather-widget.weather"    )
+local cpu_widget        = require("awesome-wm-widgets.cpu-widget.cpu-widget"     )
+local ram_widget        = require("awesome-wm-widgets.ram-widget.ram-widget"     )
+local volume_widget     = require("awesome-wm-widgets.volume-widget.volume"      )
+local spotify_widget    = require("awesome-wm-widgets.spotify-widget.spotify"    )
+local docker_widget     = require("awesome-wm-widgets.docker-widget.docker"      )
+local calendar_widget   = require("awesome-wm-widgets.calendar-widget.calendar"  )
+local net_speed         = require("awesome-wm-widgets.net-speed-widget.net-speed")
+local secrets           = require("secrets"                                      )
+
+local machi = require("layout-machi")
+
+local lain = require("lain")
+
 --terminal = config.user.terminal --"kitty" --"urxvt"
 terminal   = "urxvt"
 editor     = os.getenv("EDITOR") or "emacsclient -nc"
@@ -36,23 +48,24 @@ editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
 altkey  = "Mod1"
 ctrlkey = "Control"
+shiftkey = "Shift"
 
 local defaultlayouts = {
-   machi.default_layout,
    awful.layout.suit.tile,
    awful.layout.suit.floating,
-   lain.layout.centerwork,
-   lain.layout.termfair.center,
+--   lain.layout.centerwork,
+--   lain.layout.termfair.center,
    awful.layout.suit.spiral,
    awful.layout.suit.magnifier,
    awful.layout.suit.max,
    awful.layout.suit.max.fullscreen,
-   machi.layout.create{ new_placement_cb = machi.layout.placement.empty_then_fair },
+--   machi.layout.create{ new_placement_cb = machi.layout.placement.empty_then_fair },
    awful.layout.suit.tile.bottom,
+   machi.default_layout,
 }
 
 tag.connect_signal("request::default_layouts", function()
-    awful.layout.append_default_layouts(defaultLayouts)
+    awful.layout.append_default_layouts(defaultlayouts)
 end)
 
 lain.layout.termfair.nmaster           = 3
@@ -65,11 +78,250 @@ lain.layout.cascade.tile.extra_padding = dpi(5)
 lain.layout.cascade.tile.nmaster       = 5
 lain.layout.cascade.tile.ncol          = 2
 
-local themeName = "multicolor"
-beautiful.init(conf_dir .. "/themes/" .. themeName .. "/theme.lua")
+--- Tags -- {{{
+-- Define a tag table which hold all screen tags.
+mytags = {}
+
+-- Multi screen (desktop) tags
+mytags.desktop = {}
+mytags.desktop[1] = { "surf", "code", "play", "watch", "create" }
+mytags.desktop[2] = { "chat", "read", "listen", "system" }
+mytags.desktop[3] = { "monitor", "debug" }
+
+-- Single screen (laptop) tags
+mytags.laptop  = {}
+mytags.laptop[1] = {"chat","code","read","surf","watch","listen",
+                    "create","system","monitor"}
+
+-- Dual Screen (laptop) tags
+mytags.laptopExt = {}
+mytags.laptopExt[1] = {"chat","code","read","surf","watch","listen",
+                    "create"}
+mytags.laptopExt[2] = {"debug","monitor","system"}
+
+-- check the number of screens to determine if we are on the desktop or laptop
+if screen.count() == 3 then
+   mytags.tags = mytags.desktop
+elseif screen.count() == 2 then
+   mytags.tags = mytags.laptopExt
+else
+   mytags.tags = mytags.laptop
+end
+
+--- Widgets -- {{{
+mywidgets = {}
+
+local sep = wibox.widget { markup = "  ", align = "center", valign = "center",
+                           widget = wibox.widget.textbox }
+
+local cpu = cpu_widget({
+         width = 70,
+         step_width = 2,
+         step_spacing = 0,
+         color = '#434c5e'})
+
+local ram = ram_widget{
+   widget_show_buf = false
+}
+
+mytextclock = wibox.widget.textclock()
+
+local cw = calendar_widget({
+    theme = 'outrun',
+    placement = 'top_right',
+    start_sunday = true,
+    radius = 8,
+-- with customized next/previous (see table above)
+    previous_month_button = 1,
+    next_month_button = 3,
+})
+
+mytextclock:connect_signal("button::press",
+    function(_, _, _, button)
+        if button == 1 then cw.toggle() end
+    end)
+
+local vol = volume_widget({
+      widget_type = "arc"
+      })
+
+local spotify = spotify_widget({
+      dim_when_paused = true,
+      dim_opacity = 0.5,
+      max_length = -1,
+      font = beautiful.font
+})
+
+local netspeed = net_speed()
+
+local docker = docker_widget()
+
+local weather = weather({
+      api_key = secrets.weather_api_key,
+      coordinates = { secrets.latitude, secrets.longitude},
+      time_format_12h = true,
+      units = 'imperial',
+      both_units_widget = false,
+      font_name = 'Carter One',
+      icons = 'weather-underground-icons',
+      show_hourly_forecast = true,
+      show_daily_forecast = true,
+      icons_extension = '.png'
+})
+
+mywidgets.desktop    = { }
+mywidgets.desktop[1] = {
+   spotify,
+   sep,
+   docker,
+   sep,
+   vol,
+   netspeed,
+   cpu,
+   sep,
+   ram,
+   sep,
+   weather,
+   sep,
+   mylayoutbox,
+   layout = wibox.layout.fixed.horizontal }
+mywidgets.desktop[2] = { mylayoutbox, layout = wibox.layout.fixed.horizontal }
+mywidgets.desktop[3] = { mylayoutbox, layout = wibox.layout.fixed.horizontal }
+
+mywidgets.laptop     = { }
+mywidgets.laptop[1]  = {
+   spotify,
+   sep,
+   docker,
+   sep,
+   vol,
+   netspeed,
+   cpu,
+   sep,
+   ram,
+   sep,
+   weather,
+   sep,
+   mytextclock,
+   mylayoutbox,
+   layout = wibox.layout.fixed.horizontal }
+-- }}}
+
+if screen.count() > 1 then
+   mywidgets.widgets = mywidgets.desktop
+else
+   mywidgets.widgets = mywidgets.laptop
+end
+
+--- connect_for_each_screen -- {{{
+mypromptbox = {}
+local wibox_top = {}
+local wibox_bot = {}
+
+--- TagList -- {{{
+mytaglistbuttons = awful.util.table.join(
+   awful.button({ }, 1, awful.tag.viewonly),
+   awful.button({ modkey }, 1, awful.client.movetotag),
+   awful.button({ }, 3, awful.tag.viewtoggle),
+   awful.button({ modkey }, 3, awful.client.toggletag),
+   awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+   awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
+)
+
+-- }}}
+
+--- Tasklist -- {{{
+mytasklistbuttons = awful.util.table.join(
+   awful.button({ }, 1, function (c)
+         if c == client.focus then
+            c.minimized = true
+         else
+            -- Without this, the following
+            -- :isvisible() makes no sense
+            c.minimized = false
+            if not c:isvisible() then
+               awful.tag.viewonly(c:tags()[1])
+            end
+            -- This will also un-minimize
+            -- the client, if needed
+            client.focus = c
+            c:raise()
+         end
+   end),
+   awful.button({ }, 3, function ()
+         if instance then
+            instance:hide()
+            instance = nil
+         else
+            instance = awful.menu.clients({
+                  theme = { width = 250 }
+            })
+         end
+   end),
+   awful.button({ }, 4, function ()
+         awful.client.focus.byidx(1)
+         if client.focus then client.focus:raise() end
+   end),
+   awful.button({ }, 5, function ()
+         awful.client.focus.byidx(-1)
+         if client.focus then client.focus:raise() end
+end))
+
+awful.screen.connect_for_each_screen(function (s)
+      -- Tags
+      awful.tag(mytags.tags[s.index], s, defaultlayouts)
+
+      -- Prompt box
+      s.mypromptbox = awful.widget.prompt()
+
+      -- taglist
+      local taglist = awful.widget.taglist(s, awful.widget.taglist.filter.all,
+                                           mytaglistbuttons)
+
+      -- tasklist
+      local mytasklist =
+         awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags,
+                               mytasklistbuttons)
+
+      -- Put it all together in a wibox
+      wibox_top[s] = awful.wibar(setmetatable({ position = "top", screen = s},
+                                    {__index=wibox_args}))
+
+      local mylayouts = awful.widget.layoutbox(s)
+      mylayouts:buttons(
+         awful.util.table.join(
+            awful.button({ }, 1, function () awful.layout.inc(1, s, layouts) end),
+            awful.button({ }, 3, function () awful.layout.inc(-1, s, layouts) end),
+            awful.button({ }, 4, function () awful.layout.inc(1, s, layouts) end),
+            awful.button({ }, 5, function () awful.layout.inc(-1, s, layouts) end)))
+
+      table.insert(mywidgets.widgets[s.index], mylayouts)
+
+      wibox_top[s]:setup {
+         layout = wibox.layout.align.horizontal,
+         { -- Left side
+            taglist,
+            s.mypromptbox,
+            layout = wibox.layout.fixed.horizontal
+         },
+         nil, -- Nothing in the middle
+
+         -- right side
+         mywidgets.widgets[s.index]
+      }
+
+      wibox_bot[s] = awful.wibar(setmetatable({ position = "bottom", screen = s},
+                                    {__index=wibox_args}))
+
+      wibox_bot[s]:setup {
+         mytasklist,
+         layout = wibox.layout.flex.horizontal
+      }
+end)
+-- }}}
 
 awful.mouse.append_global_mousebindings({
-    awful.button({}, 3, function() main_menu:toggle(nil, { source = "mouse" }) end),
+--    awful.button({}, 3, function() main_menu:toggle(nil, { source = "mouse" }) end),
     awful.button({}, 4, awful.tag.viewprev),
     awful.button({}, 5, awful.tag.viewnext),
     awful.button({ modkey, altkey }, 4, function ()
@@ -83,12 +335,12 @@ awful.mouse.append_global_mousebindings({
 })
 
 awful.keyboard.append_global_keybindings({
-    awful.key({ modkey, ctrlkey }, "s", hotkeys_popup.show_help,
-              {description="show help", group="awesome"}),
-    awful.key({ modkey }, "w", function () main_menu:toggle(nil, { source = "mouse" }) end,
-              {description = "show main menu", group = "awesome"}),
-    awful.key({ modkey }, "q", function () fishlive.widget.exit_screen() end,
-              {description = "exit screen", group = "awesome"}),
+    -- awful.key({ modkey, ctrlkey }, "s", hotkeys_popup.show_help,
+    --           {description="show help", group="awesome"}),
+    -- awful.key({ modkey }, "w", function () main_menu:toggle(nil, { source = "mouse" }) end,
+    --           {description = "show main menu", group = "awesome"}),
+    -- awful.key({ modkey }, "q", function () fishlive.widget.exit_screen() end,
+    --           {description = "exit screen", group = "awesome"}),
     awful.key({ modkey }, "c", function () beautiful.menu_colorschemes_create():toggle() end,
               {description = "show colorschemes menu", group = "awesome"}),
     awful.key({ modkey }, "x", function () beautiful.menu_portrait_create():toggle() end,
@@ -204,6 +456,15 @@ awful.keyboard.append_global_keybindings({
               {description = "select next", group = "layout"}),
     awful.key({ modkey, "Shift" }, "space", function () awful.layout.inc(-1) end,
               {description = "select previous", group = "layout"}),
+})
+
+awful.keyboard.append_global_keybindings({
+      awful.key({ modkey }, "F12", function(c) awful.util.spawn("xscreensaver-command --lock") end,
+         {description = "Lock the computer"}),
+      awful.key({ modkey}, "Print", function (c) awful.util.spawn("flameshot gui") end,
+         {description = "Launch flameshot"}),
+      awful.key({ modkey, shiftkey, ctrlkey}, "r", function (c) awesome.restart() end,
+         {description = "Restart awesome"})
 })
 
 
@@ -557,7 +818,9 @@ end)
 autorun = true
 autorunProgs = {
    "xcompmgr -f -c -s",
-   "xscreensaver --no-splash"
+   "xscreensaver --no-splash",
+   "nitrogen --restore",
+   "playerctld daemon"
 }
 
 if autorun then
